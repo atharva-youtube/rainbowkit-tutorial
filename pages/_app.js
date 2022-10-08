8b8242e6d4e66448f51d84619bc370cd2780daaf
Atharva Deosthale <atharva.deosthale17@gmail.com>
2022-10-08 20:56:20 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygonMumbai, chain.goerli],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Test web3 app",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  provider,
+  connectors,
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
